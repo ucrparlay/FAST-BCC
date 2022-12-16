7e7d9a4b2858ec9844cbedf8ac558f769e668b4e
Xiaojun Dong <akatsuki6725@gmail.com>
2022-12-16 15:42:02 -0800

    feat: Use pairs add key in hash table

--- src/resizable_table.h
+++ src/resizable_table.h
@@ -22,13 +22,12 @@
 
 #pragma once
 
-#include "parlay/primitives.h"
-#include "parlay/sequence.h"
-
 #include <tuple>
 #include <unordered_set>
 #include <vector>
 
+#include "parlay/primitives.h"
+#include "parlay/sequence.h"
 #include "utilities.h"
 
 using namespace parlay;
@@ -44,67 +43,15 @@ inline size_t incrementIndex(const size_t& h, const size_t& mask) {
   return hashToRange(h + 1, mask);
 }
 
-template <class K, class V>
-struct iter_kv {
-  using T = std::tuple<K, V>;
-  K k;
-  size_t h;
-  size_t mask;
-  size_t num_probes;  // For debugging.
-  T* table;
-  K empty_key;
-  iter_kv(K _k, size_t _h, size_t _mask, T* _table, K _empty_key)
-      : k(_k),
-        h(_h),
-        mask(_mask),
-        num_probes(0),
-        table(_table),
-        empty_key(_empty_key) {}
-
-  // Finds the location of the first key
-  bool init() {
-    // while (1) {
-    for (size_t i = 0; i < mask + 1; i++) {
-      if (std::get<0>(table[h]) == empty_key) {
-        return false;
-      } else if (std::get<0>(table[h]) == k) {
-        return true;
-      }
-      h = incrementIndex(h, mask);
-    }
-    return false;
-  }
-
-  // Probes until we've found another key
-  bool has_next() {
-    h = incrementIndex(h, mask);
-    // while (1) {
-    while (num_probes < mask + 1) {  // mask +1 or mask?
-      if (std::get<0>(table[h]) == empty_key) {
-        return false;
-      } else if (std::get<0>(table[h]) == k) {
-        return true;
-      }
-      h = incrementIndex(h, mask);
-      num_probes++;
-    }
-    return false;
-  }
-
-  T next() { return table[h]; }
-};
-
-template <class K, class V, class KeyHash>
+template <class K, class KeyHash>
 class resizable_table {
  public:
-  using T = std::tuple<K, V>;
   bool table_full;
   size_t m;
   size_t mask;
   size_t ne;
-  T empty;
-  K empty_key;
-  sequence<std::tuple<K, V>> table;
+  K empty;
+  sequence<K> table;
   KeyHash key_hash;
   sequence<size_t> cts;
 
@@ -131,99 +78,27 @@ class resizable_table {
     init_counts();
   }
 
-  resizable_table(size_t _m, T _empty, KeyHash _key_hash)
+  resizable_table(size_t _m, K _empty, KeyHash _key_hash)
       // : m((size_t)1 << parlay::log2_up((size_t)(1.1 * _m))),
       : table_full(false),
-        m((size_t)1 << parlay::log2_up((size_t)_m)),
+        m((size_t)1 << parlay::log2_up((size_t)(_m * 1.1))),
         mask(m - 1),
         ne(0),
         empty(_empty),
-        empty_key(std::get<0>(empty)),
         key_hash(_key_hash) {
-    table = sequence<T>::uninitialized(m);
+    table = sequence<K>::uninitialized(m);
     clear();
     init_counts();
   }
 
-  void analyze() {
-    std::vector<size_t> probe_lengths;
-    size_t chain_length = 0;
-    for (size_t i = 0; i < m; i++) {
-      if (std::get<0>(table[i]) == empty_key) {
-        if (chain_length > 0) {
-          probe_lengths.emplace_back(chain_length);
-        }
-        chain_length = 0;
-      } else {
-        chain_length++;
-      }
-    }
-
-    std::sort(probe_lengths.begin(), probe_lengths.end());
-    std::cout << "Analyzed table, m = " << m << " ne = " << ne
-              << " num_probes = " << probe_lengths.size() << std::endl;
-    for (long k = probe_lengths.size() - 1; k > 0; k--) {
-      std::cout << probe_lengths[k] << " ";
-      if (probe_lengths.size() - k > 250) break;
-    }
-    std::cout << std::endl << "End of table analysis" << std::endl;
-  }
-
-  void maybe_resize(size_t n_inc) {
-    size_t nt = ne + n_inc;
-    if (nt > (0.25 * m)) {
-      size_t old_m = m;
-      auto old_t = std::move(table);
-      m = ((size_t)1 << parlay::log2_up((size_t)(10 * nt)));
-      if (m == old_m) {
-        return;
-      }
-      mask = m - 1;
-      ne = 0;
-      table = sequence<T>::uninitialized(m);
-      clear();
-      parallel_for(0, old_m, [&](size_t i) {
-        if (std::get<0>(old_t[i]) != empty_key) {
-          insert(old_t[i]);
-        }
-      });
-      update_nelms();
-    }
-  }
-
-  void double_size() {
-    m = 2 * m;
-    mask = m - 1;
-    update_nelms();
-    ne = 0;
-    table = sequence<T>::uninitialized(m);
-    clear();
-    table_full = false;
-  }
-
-  bool is_full() { return table_full; }
-
-  iter_kv<K, V> get_iter(K k) {
+  bool insert(K k) {
     size_t h = firstIndex(k);
-    return iter_kv<K, V>(k, h, mask, table.begin(), empty_key);
-  }
-
-  bool insert(std::tuple<K, V> kv) {
-    K& k = std::get<0>(kv);
-    V& v = std::get<1>(kv);
-    size_t h = firstIndex(k);
-    // size_t count = 0;
-    // while (1) {
-    for (size_t i = 0; i < 0.8 * m; i++) {
+    for (size_t i = 0; i < 0.9 * m; i++) {
       if (table_full) return false;
-      if (std::get<0>(table[h]) == empty_key &&
-          atomic_compare_and_swap(&table[h], empty, kv)) {
+      if (table[h] == empty && atomic_compare_and_swap(&table[h], empty, k)) {
         size_t wn = worker_id();
         cts[wn * kResizableTableCacheLineSz]++;
         return 1;
-      }
-      if (std::get<0>(table[h]) == k && std::get<1>(table[h]) == v) {
-        return false;
       }
       h = incrementIndex(h, mask);
     }
@@ -232,83 +107,20 @@ class resizable_table {
     return 0;
   }
 
-  bool insert_seq(std::tuple<K, V> kv) {
-    K& k = std::get<0>(kv);
-    V& v = std::get<1>(kv);
-    size_t h = firstIndex(k);
-    while (1) {
-      if (std::get<0>(table[h]) == empty_key) {
-        table[h] = kv;
-        return 1;
-      }
-      h = incrementIndex(h, mask);
-    }
-    return 0;
-  }
-
-  size_t num_appearances(K k) {
-    size_t h = firstIndex(k);
-    size_t ct = 0;
-    while (1) {
-      if (std::get<0>(table[h]) == k) {
-        ct++;
-      } else if (std::get<0>(table[h]) == empty_key) {
-        return ct;
-      }
-      h = incrementIndex(h, mask);
-    }
-    return 0;
-  }
-
-  bool contains(K k) {
-    size_t h = firstIndex(k);
-    while (1) {
-      if (std::get<0>(table[h]) == k) {
-        return 1;
-      } else if (std::get<0>(table[h]) == empty_key) {
-        return 0;
-      }
-      h = incrementIndex(h, mask);
-    }
-    return 0;
-  }
-
-  bool contains(K k, V v) {
-    size_t h = firstIndex(k);
-    while (1) {
-      if (std::get<0>(table[h]) == k && std::get<1>(table[h]) == v) {
-        return 1;
-      } else if (std::get<0>(table[h]) == empty_key) {
-        return 0;
-      }
-      h = incrementIndex(h, mask);
-    }
-    return 0;
-  }
-
-  template <class F>
-  void map(F& f) {
-    parallel_for(0, m, [&](size_t i) {
-      if (std::get<0>(table[i]) != empty_key) {
-        f(table[i]);
-      }
-    });
-  }
-
-  sequence<T> entries() {
-    auto pred = [&](T& t) { return std::get<0>(t) != empty_key; };
-    return filter(make_slice(table), pred);
-  }
-
   void clear() {
     parallel_for(0, m, [&](size_t i) { table[i] = empty; });
   }
+
+  sequence<K> entries() {
+    auto pred = [&](K& t) { return t != empty; };
+    return filter(make_slice(table), pred);
+  }
 };
 
-template <class K, class V, class KeyHash>
-inline resizable_table<K, V, KeyHash> make_resizable_table(
-    size_t m, std::tuple<K, V> empty, KeyHash key_hash) {
-  return resizable_table<K, V, KeyHash>(m, empty, key_hash);
+template <class K, class KeyHash>
+inline resizable_table<K, KeyHash> make_resizable_table(
+    size_t m, K empty, KeyHash key_hash) {
+  return resizable_table<K, KeyHash>(m, empty, key_hash);
 }
 
 }  // namespace gbbs
